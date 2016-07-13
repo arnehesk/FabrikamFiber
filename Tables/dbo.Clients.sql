d69ffffd193b9402c0898b5ce5658d568e6e7c05
arnehesk <arneh.eskandari@red-gate.com>
2016-07-13 15:31:43 -0700

    Merge columns

--- Tables/dbo.Clients.sql
+++ Tables/dbo.Clients.sql
@@ -5,8 +5,7 @@ CREATE TABLE [dbo].[Clients]
 [Address_City] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Address_State] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Address_Zip] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[First_Name] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Last_Name] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[FullName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Clients] ADD CONSTRAINT [PK__Customer__3214EC27A1E533D6] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
